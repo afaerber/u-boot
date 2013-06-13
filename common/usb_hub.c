91be3603cf34a2af92007ea0c12e61c9f06df0e1
Vincent Palatin <vpalatin@chromium.org>
2013-06-13 11:00:58 -0700

    usb: increase power on to power good delay
    The USB standard (USB 2.0 7.1.7.3) mentioned that the delay between the
    power ok on a hub port and the USB device attach detected should be at
    most 100ms.
    By the way, I have measured :
    - about 240ms on Patriot Supersonic Xt 8GB USB keys (black with blue XT).
    - about 370ms on Sandisk SDCZP-4096.
    - from 450 to 505ms on 64GB USB 3.0 Corsair Survivor Stealth.
    So let's extend that delay.
    
    Signed-off-by: Vincent Palatin <vpalatin@chromium.org>
    
    BRANCH=spring
    BUG=chrome-os-partner:18356
    TEST=On Spring, boot in recovery mode from various USB keys.
    
    Change-Id: Ia3e5889ac1f241b3113d09ec594bb7ef480b678a
    Reviewed-on: https://gerrit.chromium.org/gerrit/58565
    Tested-by: Vincent Palatin <vpalatin@chromium.org>
    Reviewed-by: Julius Werner <jwerner@chromium.org>
    Commit-Queue: Vincent Palatin <vpalatin@chromium.org>

--- common/usb_hub.c
+++ common/usb_hub.c
@@ -125,8 +125,8 @@ static void usb_hub_power_on(struct usb_hub_device *hub)
 		USB_HUB_PRINTF("port %d returns %lX\n", i + 1, dev->status);
 	}
 
-	/* Wait at least 100 msec for power to become stable */
-	mdelay(max(pgood_delay, (unsigned)100));
+	/* Wait at least 550 msec for power to become stable */
+	mdelay(max(pgood_delay, (unsigned)550));
 }
 
 void usb_hub_reset(void)
